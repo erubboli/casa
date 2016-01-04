3c8191d87b1bc7eb01dc06bf8f8446a6b233fc2e
Enrico Rubboli <rubboli@gmail.com>
2016-01-04 22:09:09 +0100

    move Casa into its folder and fix typos

---
+++ Casa/Casa.ino
@@ -0,0 +1,57 @@
+#include <Process.h>
+
+const int buttonPin = 6; 
+const int ringPin =  12; 
+const int ledPin = 13;
+
+const int ringLenght = 1000;
+
+int buttonState = 0;
+int ringAllowed = 1;
+int countTicks = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ringPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Bridge.begin();
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) { // use pull-up resistor   
+    if(ringAllowed == 1){
+      if(countTicks < 10){
+        countTicks++;
+      }else{
+        notify();
+        ring();    
+      }
+    }
+    
+  } else {
+    if(ringAllowed==0)
+      delay(600); //avoid to ring on release
+    ringAllowed = 1;
+    countTicks = 0;
+  }
+  
+}
+
+void notify(){
+  Process p;
+  p.runShellCommandAsynchronously("/usr/bin/notify_ring.py");
+}
+
+void ring(){
+  countTicks = 0;
+  ringAllowed = 0;
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ringPin, HIGH);
+  delay(ringLenght);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ringPin, LOW);
+}
